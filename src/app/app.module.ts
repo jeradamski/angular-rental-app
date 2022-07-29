ff3c99ae8a8aed708b9890fa840b05db24d4006d
Jurek <jer.adamski@gmail.com>
2022-07-29 20:05:56 +0200

    modifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule } from "./app-routing.module";
 
 import { AppComponent } from './app.component';
+import { BikesListComponent } from './components/bikes-list/bikes-list.component';
+import { AboutComponent } from './components/about/about.component';
+import { OrderingComponent } from './components/ordering/ordering.component';
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BikesListComponent,
+    AboutComponent,
+    OrderingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/bike.ts
@@ -0,0 +1,8 @@
+export class Bike {
+  id?: number;
+  name: string | undefined;
+  model: string | undefined;
+  size: string | undefined;
+  type: string | undefined;
+  rentalPrice: number | undefined;
+}
